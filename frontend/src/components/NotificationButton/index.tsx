95817bc93b4929aa07c302b44930a0f364f50d82
Wellington Vinicios <wellingtonv.ti.si@gmail.com>
2022-09-11 16:02:57 -0300

    Firste Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icon.svg'; {/*importando o notification-icon da pasta assets/img */}
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn"> {/*Em REACT se usa "ClassName" ao invés de "Class" */}
+            <img src={icon} alt="Notificar" /> {/*Para usar um arquivo importado no REACT se usa as chaves "{}" */}
+        </div>
+    )
+}
+
+export default NotificationButton
